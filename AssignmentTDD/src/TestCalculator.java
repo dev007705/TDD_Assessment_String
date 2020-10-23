12dbae2538e1fbd91860337b4b61f7d883c918a1
Devendra Kumar <43995712+dev007705@users.noreply.github.com>
2020-10-24 04:28:40 +0530

    Whole code after refactor Till Qs 6

--- AssignmentTDD/src/TestCalculator.java
+++ AssignmentTDD/src/TestCalculator.java
@@ -11,6 +11,7 @@ public class TestCalculator {
 		cal=new StringCalculator();
 	}
 	
+	// for empty,1 & 2 inputs #1
 	@Test
 	public void emptyString() throws Exception {
 		assertEquals(cal.Add(""),0);
@@ -23,22 +24,27 @@ public class TestCalculator {
 	public void twoString() throws Exception {
 		assertEquals(cal.Add("2,4"),6);
 	}
+	// for unknown amount of string input #2
 	@Test
 	public void unknownAmountString() throws Exception {
 		assertEquals(cal.Add("1,2,3,4"),10);
 	}
+	// allow to handle new lines #3
 	@Test
 	public void newlines() throws Exception {
 		assertEquals(cal.Add("1\n2,3"),6);
 	}
+	// allow to support different delimeter #4
 	@Test
 	public void multipledelimeter() throws Exception {
 		assertEquals(cal.Add("//;\n1;2"),3);
 	}
+	// show exception for negative number #5
 	@Test(expected = Exception.class)
-	public void negativeNumbers() throws Exception {
+	public void negativeNumber() throws Exception {
 		cal.Add("-1");
 	}
+	// show exception for multiple negative numbers #6
 	@Test(expected  = Exception.class)
 	public void multipleNegativeNumbers() throws Exception {
 		cal.Add("-1,-4,-6");
